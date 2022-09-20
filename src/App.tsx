d4a4d7b10290690c8acdcf19d4041a91836918c4
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-09-21 03:40:15 +0545

    cleanup + styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Select, SelectOption } from "./Select"
 
 const options = [
   { label: 'First', value: 1 },
-  { label: 'Second', value: 3 },
+  { label: 'Second', value: 2 },
   { label: 'Third', value: 3 },
   { label: 'Fourth', value: 4 },
   { label: 'Fifth', value: 5 },
